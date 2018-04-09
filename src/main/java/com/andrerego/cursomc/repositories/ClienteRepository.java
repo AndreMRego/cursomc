a20022f3713e22cef85b7651cb1b9ba34dd8952c
Andre Rego <andremarcelo129@gmail.com>
2018-04-09 13:42:19 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/andrerego/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/andrerego/cursomc/repositories/ClienteRepository.java
@@ -2,11 +2,14 @@ package com.andrerego.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.andrerego.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
+	@Transactional
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/andrerego/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/andrerego/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.andrerego.cursomc.domain.Cliente;
 import com.andrerego.cursomc.domain.enums.TipoCliente;
 import com.andrerego.cursomc.dto.ClienteNewDTO;
+import com.andrerego.cursomc.repositories.ClienteRepository;
 import com.andrerego.cursomc.resources.exception.FieldMessage;
 import com.andrerego.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -30,7 +37,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
-
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		// inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
