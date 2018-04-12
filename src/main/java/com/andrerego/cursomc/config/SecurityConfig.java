bd57cf09d50111abad53cc722a3c82ffdfd55237
Andre Rego <andremarcelo129@gmail.com>
2018-04-12 12:13:51 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/andrerego/cursomc/config/SecurityConfig.java
+++ src/main/java/com/andrerego/cursomc/config/SecurityConfig.java
@@ -7,21 +7,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.andrerego.cursomc.security.JWTAuthenticationFilter;
+import com.andrerego.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATHCERS= {
 			"/h2-console/**",
@@ -38,18 +49,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
-		
-		
+	
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATHCERS_GET).permitAll()
 		.antMatchers(PUBLIC_MATHCERS).permitAll()
 		.anyRequest().authenticated();
 		
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		//não criar sessão de usuario
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+	}
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
