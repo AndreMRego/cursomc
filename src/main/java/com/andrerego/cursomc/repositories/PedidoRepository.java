a2ee9b971dbea6ec8efb4aaf0165ed10faee6f42
Andre Rego <andremarcelo129@gmail.com>
2018-04-12 13:18:19 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/andrerego/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/andrerego/cursomc/repositories/PedidoRepository.java
@@ -1,12 +1,18 @@
 package com.andrerego.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.andrerego.cursomc.domain.Cliente;
 import com.andrerego.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente,Pageable pageRequest);
 
 }
--- src/main/java/com/andrerego/cursomc/services/PedidoService.java
+++ src/main/java/com/andrerego/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.andrerego.cursomc.domain.Cliente;
 import com.andrerego.cursomc.domain.ItemPedido;
 import com.andrerego.cursomc.domain.PagamentoComBoleto;
 import com.andrerego.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.andrerego.cursomc.domain.enums.EstadoPagamento;
 import com.andrerego.cursomc.repositories.ItemPedidoRepository;
 import com.andrerego.cursomc.repositories.PagamentoRepository;
 import com.andrerego.cursomc.repositories.PedidoRepository;
+import com.andrerego.cursomc.security.UserSS;
+import com.andrerego.cursomc.services.exceptions.AuthorizationException;
 import com.andrerego.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -67,6 +73,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
