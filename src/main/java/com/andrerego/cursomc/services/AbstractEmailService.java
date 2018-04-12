4396de5d5b967b42f7c57a23134a2c2c1b252847
Andre Rego <andremarcelo129@gmail.com>
2018-04-12 10:04:33 -0300

    Email HTML

--- src/main/java/com/andrerego/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/andrerego/cursomc/services/AbstractEmailService.java
@@ -2,8 +2,16 @@ package com.andrerego.cursomc.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.andrerego.cursomc.domain.Pedido;
 
@@ -12,6 +20,14 @@ public abstract class AbstractEmailService implements EmailService {
 	//pegar valor do aplication.properties
 	@Value("${default.sender}")
 	private String sender;
+	
+	//tranformar html em forma de string
+	@Autowired
+	private TemplateEngine templateEngine;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
@@ -32,6 +48,35 @@ public abstract class AbstractEmailService implements EmailService {
 		//corpo do email
 		sm.setText(obj.toString());
 		return sm;
+	}
+	
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context();
+		//injetar obj pedido com o apelido de "pedido"
+		context.setVariable("pedido", obj);
+		return templateEngine.process("email/confirmacaoPedido", context);
+		
+	}
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		try {
+		MimeMessage mm = prepareMimeMessageFromPedido(obj);
+		sendHtmlEmail(mm);
+		}catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+			MimeMessage mimeMessage= javaMailSender.createMimeMessage();
+			MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+			mmh.setTo(obj.getCliente().getEmail());
+			mmh.setFrom(sender);
+			mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
+			mmh.setSentDate(new Date(System.currentTimeMillis()));
+			//pegar o corpo do email e true para afirmar que é html
+			mmh.setText(htmlFromTemplatePedido(obj),true);
+		return mimeMessage;
 	}
 
 }
